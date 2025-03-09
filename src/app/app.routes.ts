57c92153889b72137515885bdb91459bfc4a2a9f
nagoorsky <112877495+nagoorsky@users.noreply.github.com>
2025-03-09 10:03:24 +0100

    feat(cats): auth guard, login session in local storage (#4)
    Co-authored-by: Paweł Nagórski <panagors@assecobs.pl>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,19 @@
 import { Routes } from '@angular/router';
+import { authGuard, loginGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
+    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent),
+    canActivate: [loginGuard]
   },
   {
     path: 'list',
-    loadComponent: () => import('./components/list/list.component').then(m => m.ListComponent)
+    loadComponent: () => import('./components/list/list.component').then(m => m.ListComponent),
+    canActivate: [authGuard]
   },
   {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
+    path: '**',
+    redirectTo: '/login'
   }
 ];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,20 +10,31 @@ export class AuthService {
   private readonly router = inject(Router);
   private readonly VALID_USERNAME = '1';
   private readonly VALID_PASSWORD = '1';
+  private readonly IS_LOGGED_KEY = 'isLoggedIn';
 
-  isLoggedIn = signal<boolean>(false);
+  isLoggedIn = signal<boolean>(this.checkLoginStatus());
+
+  private checkLoginStatus(): boolean {
+    return localStorage.getItem(this.IS_LOGGED_KEY) === 'true';
+  }
 
   login(username: string, password: string): Observable<boolean> {
     const isValid =
       username === this.VALID_USERNAME && password === this.VALID_PASSWORD;
     return of(isValid).pipe(
       delay(1000), // Simulate API delay
-      tap(() => this.isLoggedIn.set(isValid))
+      tap(() => {
+        this.isLoggedIn.set(isValid);
+        if (isValid) {
+          localStorage.setItem(this.IS_LOGGED_KEY, 'true');
+        }
+      })
     );
   }
 
   logout(): void {
     this.isLoggedIn.set(false);
+    localStorage.removeItem(this.IS_LOGGED_KEY);
     this.router.navigate(['/login']);
   }
 }
